65b7ef8284fc02820f45438ef9511a5ef2d6a62f
Sameer <114623580+SamSwnshi@users.noreply.github.com>
2025-09-23 23:18:11 +0530

    home page updated

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import UserForm from "./UserForm";
-import axios from "axios";
+import { getUsers } from "../services/api";
 import UserTable from "./UserTable";
 const API_URL = "https://jsonplaceholder.typicode.com/users";
 const Home = () => {
@@ -9,38 +9,26 @@ const Home = () => {
     const [error, setError] = useState('')
 
     useEffect(() => {
-        const fetchData = async () => {
-            setIsLoading(true);
+        const fetchUser = async () => {
             try {
-                const response = await axios.get(API_URL);
+                const response = await getUsers()
                 const data = response.data;
-                console.log(data);
-                // console.log(data[0].company.name)
-                const users = data.map((user, idx) => ({
-                    id: user.id,
-                    company: user.company.name,
-                    email: user.email,
-                    username: user.username,
-                    website: user.website,
-                    phone: user.phone,
-                    city:user.address.city
-                }));
-                setAllUsers(users)
+                console.log(data)
+                setAllUsers(data)
             } catch (err) {
-                setError("Failed to Fetch the Users, Please Try Again!")
-            } finally {
-                setIsLoading(false)
+                setError('Failed to fetch users. Please try again.');
+                console.error(err);
             }
         }
-        fetchData();
-    }, []);
+        fetchUser()
+    },[])
     return (
         <div className="container mx-auto p-4">
             <div className="flex justify-end mb-4">
                 <button>Add New User</button>
             </div>
             <UserForm />
-            <UserTable users={allUsers}/>
+            <UserTable users={allUsers} />
         </div>
     );
 };
